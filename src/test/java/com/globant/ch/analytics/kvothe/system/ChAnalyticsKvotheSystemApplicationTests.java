c9b81dcd6a04ab64b8c1984cc55e864e1bbbe60e
Cynthia Abarca A <cynthia.abarca@gmail.com>
2022-04-25 18:44:31 -0400

    initial proyect maven-spring

---
+++ src/test/java/com/globant/ch/analytics/kvothe/system/ChAnalyticsKvotheSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globant.ch.analytics.kvothe.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChAnalyticsKvotheSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
